d3a099cc5c29c1f276afc955a533ef2b43afd12a
Laurie Waller <lauriewaller7@gmail.com>
2021-05-08 12:43:45 -0700

    write models

---
+++ OrderTracker/Models/Order.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Order
+  {
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public int Price { get; set; }
+    public string Date { get; set; }
+    public int Id { get; }
+
+    private static List<Order> _instances = new List<Order> {};
+
+    public Order (string orderTitle, string orderDescription, int orderPrice, string orderDate)
+    {
+      Title = orderTitle;
+      Description = orderDescription;
+      Price = orderPrice;
+      Date = orderDate;
+      _instances.Add(this);
+      Id = _instances.Count;
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static Order Find(int searchId)
+    {
+      return _instances[searchId - 1];
+    }
+  }
+}
