24b0c0455f756f24a1cf6065342d99bef538e409
Laurie Waller <lauriewaller7@gmail.com>
2021-05-08 22:32:07 -0700

    pass tests for GetAll and getId

--- OrderTrackerTests/ModelTests/OrderTests.cs
+++ OrderTrackerTests/ModelTests/OrderTests.cs
@@ -10,7 +10,7 @@ namespace OrderTracker.Tests
   {
     public void Dispose()
     {
-      Vendor.ClearAll();
+      Order.ClearAll();
     }
 
     [TestMethod]
@@ -19,5 +19,46 @@ namespace OrderTracker.Tests
       Order newOrder = new Order("title", "description", 2, "date");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      //Arrange
+      string orderTitle1 = "tacos";
+      string orderDescription1 = "all the tacos";
+      int orderPrice1 = 2;
+      string orderDate1 = "7.25.22";
+      string orderTitle2 = "ice cream";
+      string orderDescription2 = "rocky road";
+      int orderPrice2 = 4;
+      string orderDate2 = "7.25.22";
+      Order newOrder1 = new Order(orderTitle1, orderDescription1, orderPrice1, orderDate1);
+      Order newOrder2 = new Order(orderTitle2, orderDescription2, orderPrice2, orderDate2);
+      List<Order> newList = new List<Order> { newOrder1, newOrder2 };
+
+      //Act
+      List<Order> result = Order.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+        [TestMethod]
+    public void GetId_OrdersInstantiateWithAnIdAndGetterReturns_Int()
+    {
+      //Arrange
+      string orderTitle1 = "tacos";
+      string orderDescription1 = "all the tacos";
+      int orderPrice1 = 2;
+      string orderDate1 = "7.25.22";
+      Order newOrder = new Order(orderTitle1, orderDescription1, orderPrice1, orderDate1);
+
+      //Act
+      int result = newOrder.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
+    
   }
 }
