02f1e8165cffab4c1513ffb2c5d0fed85e1ae71d
Laurie Waller <lauriewaller7@gmail.com>
2021-05-08 21:38:39 -0700

    write passing for Vendor constructor

---
+++ OrderTrackerTests/ModelTests/VendorTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("test Vendor name", "test vendor description");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
