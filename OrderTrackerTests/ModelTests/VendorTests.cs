b12e6933aa7bd8867a31f2eb3de24ecab7bd519c
Laurie Waller <lauriewaller7@gmail.com>
2021-05-08 21:45:22 -0700

    pass test for Vendor Id property

--- OrderTrackerTests/ModelTests/VendorTests.cs
+++ OrderTrackerTests/ModelTests/VendorTests.cs
@@ -49,5 +49,20 @@ namespace OrderTracker.Tests
       //Assert
       Assert.AreEqual(description, result);
     }
+
+    [TestMethod]
+    public void GetId_ReturnsVendorId_Int()
+    {
+      //Arrange
+      string name = "Test Vendor";
+      string description = "Test Description";
+      Vendor newVendor = new Vendor(name, description);
+
+      //Act
+      int result = newVendor.Id;
+
+      //Assert
+      Assert.AreEqual(1, result);
+    }
   }
 }
